fa04706fa0a456b39b66b1ee8ca79b517cbd1074
Jude <lifangxv@gmail.com>
2014-03-07 21:53:49 +0800

    A simple Asmx

---
+++ webservice/App_Code/DataTransObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+///DataTransObject 的摘要说明
+/// </summary>
+public class DataTransObject
+{
+    private String clientName;
+    private String message;
+
+    public String ClientName
+    {
+        get { return clientName; }
+        set { clientName = value; }
+    }
+
+    public String Message
+    {
+        get { return message; }
+        set { message = value; }
+    }
+
+    public DataTransObject()
+    {
+        //
+        //TODO: 在此处添加构造函数逻辑
+        //
+        clientName = string.Empty;
+        message = string.Empty;
+    }
+
+}
---
+++ webservice/App_Code/Service.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。 
+// [System.Web.Script.Services.ScriptService]
+
+public class MyService : System.Web.Services.WebService
+{
+    public MyService()
+    {
+
+        //如果使用设计的组件，请取消注释以下行 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public string SendMessage(DataTransObject dataTransObject)
+    {
+        if (string.IsNullOrEmpty(dataTransObject.ClientName))
+        {
+            return "Who are you?";
+        }
+        if (string.IsNullOrEmpty(dataTransObject.Message))
+        {
+            return "What are you say";
+        }
+        return dataTransObject.ClientName + " Send :" + dataTransObject.Message;
+    }
+
+}
